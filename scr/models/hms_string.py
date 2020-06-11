9a6e59cb97a968ff1defa06b298de283be1b48f6
Fanli Zhou <flizhou@gmail.com>
2020-06-10 19:55:22 -0700

    Added hms_string function for format

---
+++ scr/models/hms_string.py
@@ -0,0 +1,10 @@
+# Author: Fanli Zhou
+# Date: 2020-06-10
+#
+# This script defines hms_string
+
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return f"{h}:{m:>02}:{s:>05.2f}"
